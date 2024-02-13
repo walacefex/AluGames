45adf698348f10ead3207204c1525e200d669490
walacefex <walacefex01@gmail.com>
2024-02-13 18:26:45 -0300

    Creating class Jogo

---
+++ src/main/kotlin/Jogo.kt
@@ -0,0 +1,11 @@
+class Jogo {
+    var titulo = ""
+    var capa = ""
+    val descricao = ""
+    override fun toString(): String {
+        return "Meu jogo:'\n" +
+                "Título: $titulo \n" +
+                "Capa: $capa \n" +
+                "Descricao: $descricao"
+    }
+}
