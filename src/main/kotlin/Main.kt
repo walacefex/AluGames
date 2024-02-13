e0ffd1f4607245a7953a1de067f39a1f6f2fe879
walacefex <walacefex01@gmail.com>
2024-02-13 18:34:50 -0300

    Pass value in props titulo and capa

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -15,9 +15,7 @@ fun main() {
     val json = response.body()
     println(json)
 
-    val meuJogo = Jogo()
-    meuJogo.titulo = "Batman: Arkham Asylum Game of the Year Edition"
-    meuJogo.capa = "https:\\/\\/cdn.cloudflare.steamstatic.com\\/steam\\/apps\\/35140\\/capsule_sm_120.jpg?t=1681938587"
+    val meuJogo = Jogo("Batman","Capa")
 
     println(meuJogo)
 }
