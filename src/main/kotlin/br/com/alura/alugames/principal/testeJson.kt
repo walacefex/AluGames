fe83146f5718257f7d896d3784506afc6bf80d53
walacefex <walacefex01@gmail.com>
2024-02-15 02:02:34 -0300

    Consuming values from other Apis and creating another class to treat this data

---
+++ src/main/kotlin/br/com/alura/alugames/principal/testeJson.kt
@@ -0,0 +1,9 @@
+package br.com.alura.alugames.principal
+
+import br.com.alura.alugames.servicos.ConsumoApi
+
+fun main(){
+    val consumo = ConsumoApi()
+    val listaGamers = consumo.buscaGamers()
+    println(listaGamers)
+}
