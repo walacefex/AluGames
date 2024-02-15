ed88474e6209886f80212b83923d4a6baf43a892
walacefex <walacefex01@gmail.com>
2024-02-15 00:10:22 -0300

    Creating a treat for email using regex

--- src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
+++ src/main/kotlin/br/com/alura/alugames/modelo/Gamer.kt
@@ -19,7 +19,12 @@ data class Gamer(var nome:String, var email:String){
         this.usuario = usuario
         criarIdInterno()
     }
-
+    init {
+        if(nome.isNullOrBlank()){
+            throw IllegalArgumentException("Nome está em branco")
+        }
+        this.email = validarEmail()
+    }
     override fun toString(): String {
         return "Gamer(nome='$nome', email='$email', dataNascimento=$dataNascimento, usuario=$usuario, idInterno=$idInterno)"
     }
@@ -29,5 +34,14 @@ data class Gamer(var nome:String, var email:String){
         val tag = String.format("%04d", numero)
 
         idInterno = "$usuario#$tag"
+    }
+
+    fun validarEmail(): String{
+        val regex = Regex("^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}$")
+        if(regex.matches(email)){
+            return email
+        } else{
+            throw IllegalArgumentException("Email inválido")
+        }
     }
 }
